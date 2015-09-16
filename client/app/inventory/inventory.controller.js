f57aa4e8833b73746682368b4326068d40af53b9
Robin Dang <dang.robin@gmail.com>
2015-09-15 22:38:55 -0400

    Added smart table

--- client/app/inventory/inventory.controller.js
+++ client/app/inventory/inventory.controller.js
@@ -1,6 +1,26 @@
 'use strict';
 
 angular.module('tdpharmaClientApp')
-  .controller('InventoryCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('InventoryCtrl', ['Resource', function (service) {
+
+    var ctrl = this;
+
+    this.displayed = [];
+
+    this.callServer = function callServer(tableState) {
+
+      ctrl.isLoading = true;
+
+      var pagination = tableState.pagination;
+
+      var start = pagination.start || 0;     // This is NOT the page number, but the index of item in the list that you want to use to display the table.
+      var number = pagination.number || 10;  // Number of entries showed per page.
+
+      service.getPage(start, number, tableState).then(function (result) {
+        ctrl.displayed = result.data;
+        tableState.pagination.numberOfPages = result.numberOfPages;//set the number of pages so the pagination can update
+        ctrl.isLoading = false;
+      });
+    };
+
+  }]);
