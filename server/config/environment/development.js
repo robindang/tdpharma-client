55828868ef05e4eb3e51a1b435baa05aae2af5e9
Robin Dang <dang.robin@gmail.com>
2016-01-08 13:48:52 -0500

    Removed mongo project dependencies.

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -3,10 +3,5 @@
 // Development specific configuration
 // ==================================
 module.exports = {
-  // MongoDB connection options
-  mongo: {
-    uri: 'mongodb://localhost/tdpharmaclient-dev'
-  },
-
   seedDB: true
 };
