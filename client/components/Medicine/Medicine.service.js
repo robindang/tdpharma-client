6e4ebfaa785a874d99d8173a1b3ae430e8e76aa8
Robin Dang <dang.robin@gmail.com>
2016-01-11 12:29:29 -0500

    Fixed deprecated warnings

--- client/components/Medicine/Medicine.service.js
+++ client/components/Medicine/Medicine.service.js
@@ -3,7 +3,7 @@
 angular.module('tdpharmaClientApp')
   .factory('Medicine', Medicine);
 
-Medicine.$inject = ['$resource', APP_CONFIGURATION];
+Medicine.$inject = ['$resource', 'APP_CONFIGURATION'];
 
 function Medicine($resource, APP_CONFIGURATION) {
   return $resource(APP_CONFIGURATION.API_V1_URL+'medicines/:id/:controller', {
