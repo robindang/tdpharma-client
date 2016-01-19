166e692a407a45db61e0f1132253285f96b52909
Robin Dang <dang.robin@gmail.com>
2016-01-19 08:15:08 -0500

    Fixed resolve filter spec

--- client/components/filters/resolve/resolve.filter.spec.js
+++ client/components/filters/resolve/resolve.filter.spec.js
@@ -13,12 +13,13 @@ describe('Filter: resolve', function () {
 
   it('should return the input prefixed with "resolve filter:"', function () {
     var text = 'angularjs';
-    var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-      '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
-      '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
-      '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
-      '([?][;&a-z\d%_.~+=-]*)?'+ // query string
-      '(\#[-a-z\d_]*)?$','i'); // fragment locater
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3})|'+ // OR ip (v4) address
+      'localhost)'+ // OR localhost
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '([?][;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locater
     expect(resolve(text)).toMatch(pattern);
   });
 
