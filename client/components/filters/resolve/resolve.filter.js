f3d164188b644d0787572da6ce22f04ff7f4e032
Robin Dang <dang.robin@gmail.com>
2016-01-25 10:29:11 -0500

    Fixed bad HTTP request when no url passed to resolve filter

--- client/components/filters/resolve/resolve.filter.js
+++ client/components/filters/resolve/resolve.filter.js
@@ -7,6 +7,7 @@ resolve.$inject = ['util', 'APP_CONFIGURATION'];
 
 function resolve(util, APP_CONFIG) {
     return function(url) {
+      if (!url) return '#';
       if (util.isAbsoluteUrl(url)) return url;
       return APP_CONFIG.SERVER_DEFAULT_PICTURE_ENDPOINT+'/'+url;
     };
