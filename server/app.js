dffdaf066750946e3276e0b5ab72dd50482d24a2
Tri Dang <quangtrister@gmail.com>
2016-06-16 13:02:00 -0400

    set app server back to the previous configuration for heroku to test https

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
-var https = require('https');
+// var https = require('https');
 var http = require('http');
 var config = require('./config/environment');
 var fs = require('fs');
@@ -16,14 +16,14 @@ var fs = require('fs');
 // Populate DB with sample data
 if(config.seedDB) { require('./config/seed'); }
 
-// Setup server. Only needed for development
+// Setup server. Only needed for development 
  // var options = {
  //      key: fs.readFileSync('./server/config/develop_ssl/server.key'),
  //      cert: fs.readFileSync('./server/config/develop_ssl/server.crt')
  //   };
 var app = express();
-var server = https.createServer(options, app);
-var server_no_ssl = http.createServer(app);
+var server = http.createServer(app);
+// var server = https.createServer(options, app);
 
 app.use('*',function(req,res,next){  
 	var isSecure = req.secure || req.headers['x-forwarded-proto'] == 'https';
@@ -43,8 +43,6 @@ require('./routes')(app);
 server.listen(config.port, config.ip, function () {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
-
-server_no_ssl.listen(9001, config.ip);
 
 // Expose app
 exports = module.exports = app;
