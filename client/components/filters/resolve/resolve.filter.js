7ad3ed0d916aeee6982c5ebfde160af596fdbcad
Tri Dang <quangtrister@gmail.com>
2016-09-06 22:00:57 -0400

    for new paperclip version

--- client/components/filters/resolve/resolve.filter.js
+++ client/components/filters/resolve/resolve.filter.js
@@ -8,7 +8,8 @@ resolve.$inject = ['util', 'APP_CONFIGURATION'];
 function resolve(util, APP_CONFIG) {
     return function(url) {
       if (!url) {return '#';}
-      if (util.isAbsoluteUrl(url)) {return url;}
+      // With paperclip 5 and aws sdk 2, image url is also return as relative path
+      if (url.indexOf('s3.amazonaws') >= 0) {return url;}            
       return APP_CONFIG.SERVER_DEFAULT_PICTURE_ENDPOINT+'/'+url;
     };
 };
