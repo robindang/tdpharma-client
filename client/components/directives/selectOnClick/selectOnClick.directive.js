da3604d5ab63094314ca9c41a0ec02ab7ab81b45
Robin Dang <dang.robin@gmail.com>
2016-09-06 20:09:40 -0400

    Added directive selectOnClick to select input box text on selection. Changed price to input box in the checkout page. Styling needs some work.

---
+++ client/components/directives/selectOnClick/selectOnClick.directive.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('tdpharmaClientApp')
+  .directive('selectOnClick', ['$window', function ($window) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        element.on('click', function () {
+          if (!$window.getSelection().toString()) {
+            // Required for mobile Safari
+            this.setSelectionRange(0, this.value.length)
+          }
+        });
+      }
+    };
+  }]);
