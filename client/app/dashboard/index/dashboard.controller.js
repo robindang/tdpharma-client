8b8d413a60fcca1bf10d76c8da357ab09f7f3d74
Robin Dang <dang.robin@gmail.com>
2016-09-07 23:56:10 -0400

    Started stubbing out dashboard.

--- client/app/dashboard/index/dashboard.controller.js
+++ client/app/dashboard/index/dashboard.controller.js
@@ -3,7 +3,38 @@
 angular.module('tdpharmaClientApp')
   .controller('DashboardCtrl', DashboardCtrl);
 
-DashboardCtrl.$inject = ['$scope'];
+DashboardCtrl.$inject = ['$scope', 'OrderSearch'];
 
-function DashboardCtrl($scope) {
+function DashboardCtrl($scope, OrderSearch) {
+  var ctrl = this;
+  // todo(robindang): fix hardcoded number of items
+  OrderSearch.getPage(
+    0, // page
+    10000, // max item count
+    {
+      sale: true,
+      // todo(robindang): change min date to start of the day
+      min_date: moment().subtract(1, 'day')
+    })
+  .then(
+    function(result){
+      ctrl.sales = result.data;
+      console.log(ctrl.sales);
+    });
+
+  ctrl.chartConfig = {
+    options: {
+      chart: {
+        type: 'spline'
+      }
+    },
+    series: [{
+      data: [10, 15, 12, 8, 7]
+    }],
+    title: {
+      text: 'Sales'
+    },
+
+    loading: false
+  };
 }
